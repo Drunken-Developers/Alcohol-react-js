ddbf9646fb83fbf70103701c6a81cf0a687abe1d
juhyejin <juhyejin.dev@gmail.com>
2022-10-24 21:35:15 +0900

    Feat: Appbar component 제작, 커뮤니티 파일 연결

---
+++ src/pages/Community/CommunityList.js
@@ -0,0 +1,12 @@
+import BoardCard from "../../components/Common/BoardCard";
+import AppBar from "../../components/Common/AppBar/AppBar";
+
+export default function CommunityList(){
+    return (
+        <div>
+            <AppBar variant={'back'} />
+            커뮤니티 리스트
+            <BoardCard/>
+        </div>
+    )
+}
