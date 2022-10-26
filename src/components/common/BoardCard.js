1b864c50ca84d010b51ff3e4181b845eeff61ee0
juhyejin <juhyejin.dev@gmail.com>
2022-10-26 15:32:02 +0900

    Revert "Feat: 게시글 클릭시 상세화면이동"
    This reverts commit 0a57279f29730f1e4454902300423c33e80b1466.

--- src/components/common/BoardCard.js
+++ src/components/common/BoardCard.js
@@ -1,20 +1,20 @@
 import '../../assets/css/Community.css'
 import dummy from '../../db/CommunityList.json'
-import {Link} from "react-router-dom";
-
-
 
 export default function BoardCard(){
+    console.log(dummy)
+
+    function clickCard(data){
+        alert(JSON.stringify(data))
+    }
     return (
         <div>
             {dummy.board.map((board)=>(
-                <Link to={`/community-list/${board.id}`} state={board} key={board.id} >
-                <div className={'board_card'}>
+                <div key={board.id} className={'board_card'} onClick={()=>clickCard(board)}>
                     <h3>{board.title}</h3>
                     <div>{board.content}</div>
                     좋아요 수 : {board.like} 댓글 수 : {board.comment.length}
                 </div>
-                </Link>
             ))}
         </div>
     )
