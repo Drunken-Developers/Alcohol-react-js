38d8d7f74ace3fad211db13f23b88f984675700f
jungi <joongi1217@gmail.com>
2022-10-26 14:49:04 +0900

    feat: appbar 검색 기능 토글 구현

--- src/components/common/app-bar/SearchAppbar.js
+++ src/components/common/app-bar/SearchAppbar.js
@@ -1,14 +1,32 @@
 import { useNavigate } from "react-router-dom";
 import { FiSearch } from "react-icons/fi";
+import { useState } from "react";
+import { click } from "@testing-library/user-event/dist/click";
+import { Button } from "bootstrap";
 
 const SeachAppbar = () => {
     const navigate = useNavigate();
+    const [toggleSerach, UseToggle] = useState(false)
     const goSearch = () => {
-        navigate('/search');
+        navigate(`/${text}`);
     };
+    
+    const [text, setText] = useState("");
+    const onChange = (e) => {
+        setText(e.target.value);
+    }
+    const onSearch = () => {
+        UseToggle(!toggleSerach)
+        console.log(toggleSerach)
+        setText('');
+    }
     return(
-        <FiSearch style={{float: 'right'}} className="mainHeader" size={30} onClick={goSearch} />
-        // <ModalPopup />
+        <>
+            <FiSearch style={{float: 'right'}} className="mainHeader" size={30} onClick={onSearch} />
+            { toggleSerach && <input onChange = {onChange} value={text} placeholder="검색어를 입력하세요" />}
+            { toggleSerach && <button onClick={goSearch} >검색</button>}
+            { toggleSerach && <>검색 내용: {text}</>}
+        </>
     )
 }
 
