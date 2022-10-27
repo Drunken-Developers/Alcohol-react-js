f17a2160018d86b87535ab119990ff6276c3e5a9
jungi <joongi1217@gmail.com>
2022-10-27 17:35:00 +0900

    feat: 검색 기능 추가

--- src/pages/community/CommunityList.js
+++ src/pages/community/CommunityList.js
@@ -1,10 +1,14 @@
-import BoardCard from "../../components/common/BoardCard.js";
+// import BoardCard from "../../components/common/BoardCard.js";
+import BoardCardContent from "../../components/common/BoardCardContent.js";
+import dummy from "../../db/CommunityList.json"
 
 export default function CommunityList(){
     return (
         <div>
             커뮤니티 리스트
-            <BoardCard/>
+            {dummy.board.map((board)=>(
+                <BoardCardContent state={board} key={board.id} />
+            ))}
         </div>
     )
 }
