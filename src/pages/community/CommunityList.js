328aa7c9af07ba657747157b8fa8765ced4acd48
juhyejin <juhyejin.dev@gmail.com>
2022-11-03 21:36:00 +0900

    Feat: 글쓰기 버튼 제작

--- src/pages/community/CommunityList.js
+++ src/pages/community/CommunityList.js
@@ -1,17 +1,23 @@
 
 import BoardCard from "../../components/common/BoardCard.js";
-import {Outlet} from "react-router-dom";
+import {Link, Outlet} from "react-router-dom";
 import dummy from "../../db/CommunityList.json"
 import BoardCardContent from "../../components/common/BoardCardContent.js";
+import '../../assets/css/Community.css'
 
 export default function CommunityList(){
     return (
-        <div>
+        <div className="community-main">
             <Outlet/>
             커뮤니티 리스트
             {dummy.board.map((board)=>(
                 <BoardCardContent state={board} key={board.id} />
             ))}
+            <div className='write-btn'>
+                <button type='button'>
+                    <Link to='/write'>글쓰기</Link>
+                </button>
+            </div>
         </div>
     )
 }
--- src/pages/community/Write.js
+++ src/pages/community/Write.js
@@ -1,22 +1,21 @@
-import React from "react";
+import React, {useState} from "react";
 
 // const Write = (props) => {
 function Write(props) {
+    const [write, inputText] = useState('');
+    const inputWrite = (e) => {
+        console.log(e)
+        console.log(e.target.value)
+    }
     return (
         <article>
             <div> 글쓰기 </div>
-            <form onSubmit={(event) => {
-                event.preventDefault();
-                const title = event.target.title.value;
-                const body = event.target.body.value;
-                props.onCreate(title, body);
-            }}
-            >
+            <form>
                 <div>
                     <input type="text" name="title" placeholder="제목" />
                 </div>
                 <div>
-                    <textarea name="body" placeholder="내용"></textarea>
+                    <textarea name="body" placeholder="내용" onInput={inputWrite}></textarea>
                 </div>
                 <div>
                     <input type="submit" value="작성"></input>
